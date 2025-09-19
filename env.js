a369e23826f3eea5af09ee8553080f7ad3ded1e3
AMSANJEEV28 <amsanjeev28@gmail.com>
2025-09-20 00:34:27 +0530

    UPDATE

---
+++ env.js
@@ -0,0 +1,14 @@
+// env.js
+// ⚠️ Do NOT push this file to GitHub. Add it to .gitignore
+
+window._env_ = {
+  FIREBASE_API_KEY: "AIzaSyAR42iwcRLkywkiu83Oix7S5z_YxBq5rgc",
+  FIREBASE_AUTH_DOMAIN: "waste-detective-7214b.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "waste-detective-7214b",
+  FIREBASE_STORAGE_BUCKET: "waste-detective-7214b.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "1047902917063",
+  FIREBASE_APP_ID: "1:1047902917063:web:f5442cb1b5ae738b8a89e2",
+  FIREBASE_MEASUREMENT_ID: "G-4HV7GFE612"
+};
+
+console.log("Environment variables loaded:", window._env_);

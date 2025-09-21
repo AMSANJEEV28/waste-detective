97ea1c0ea6e2fee89370564a527cd085f0ace415
AMSANJEEV28 <amsanjeev28@gmail.com>
2025-09-21 20:43:09 +0530

    Events

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAR42iwcRLkywkiu83Oix7S5z_YxBq5rgc",
-  authDomain: "waste-detective-7214b.firebaseapp.com",
-  projectId: "waste-detective-7214b",
-  storageBucket: "waste-detective-7214b.firebasestorage.app",
-  messagingSenderId: "1047902917063",
-  appId: "1:1047902917063:web:59257f2f5156fea58a89e2",
-  measurementId: "G-RM5VFX0XMQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- env.js
+++ env.js
@@ -1,14 +1,13 @@
 // env.js
-// ⚠️ Do NOT push this file to GitHub. Add it to .gitignore
-
+// This file will read environment variables set in Netlify
 window._env_ = {
-  FIREBASE_API_KEY: "AIzaSyAR42iwcRLkywkiu83Oix7S5z_YxBq5rgc",
-  FIREBASE_AUTH_DOMAIN: "waste-detective-7214b.firebaseapp.com",
-  FIREBASE_PROJECT_ID: "waste-detective-7214b",
-  FIREBASE_STORAGE_BUCKET: "waste-detective-7214b.firebasestorage.app",
-  FIREBASE_MESSAGING_SENDER_ID: "1047902917063",
-  FIREBASE_APP_ID: "1:1047902917063:web:f5442cb1b5ae738b8a89e2",
-  FIREBASE_MEASUREMENT_ID: "G-4HV7GFE612"
+  FIREBASE_API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: import.meta.env.VITE_FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 console.log("Environment variables loaded:", window._env_);
